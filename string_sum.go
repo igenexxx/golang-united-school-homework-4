a4b465b0883b8a31a458bab961ba8371ff3df6a8
Zhenya Sikirzhitsky <zhenya_sikirzhitsky@epam.com>
2022-05-14 10:54:39 +0400

    feature/parse-expression: add solution for expression parsing

--- string_sum.go
+++ string_sum.go
@@ -2,6 +2,10 @@ package string_sum
 
 import (
 	"errors"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 //use these errors as appropriate, wrapping them with fmt.Errorf function
@@ -22,6 +26,74 @@ var (
 //
 // Use the errors defined above as described, again wrapping into fmt.Errorf
 
-func StringSum(input string) (output string, err error) {
+func getMatches(expression string) (error, []string) {
+	var err error = nil
+
+	if strings.Trim(expression, " ") == "" {
+		return fmt.Errorf("%s", errorEmptyInput), nil
+	}
+
+	invalidExpr := regexp.MustCompile(`[^ \d+-]`)
+
+	if invalidExpr.MatchString(expression) {
+		return fmt.Errorf("invalid character"), nil
+	}
+
+	cleanExprRegex := regexp.MustCompile(`[^\d+-]`)
+	matchesRegex := regexp.MustCompile(`(\+?-?\d+)([-+])(\+?-?\d+)`)
+
+	cleanExpr := cleanExprRegex.ReplaceAllString(expression, "")
+	matches := matchesRegex.FindStringSubmatch(cleanExpr)
+
+	if matches == nil {
+		return fmt.Errorf("no matches"), nil
+	}
+
+	if len(matches[1:]) != 3 {
+		return fmt.Errorf("%s", errorNotTwoOperands), nil
+	}
+
+	if matches != nil {
+		return nil, matches[1:]
+	}
+
+	return err, nil
+}
+
+func sum(a, b int) int {
+	return a + b
+}
+
+func subtr(a, b int) int {
+	return a - b
+}
+
+func parse(expression string) (string, error) {
+	err, result := getMatches(expression)
+
+	if err != nil {
+		return "", err
+	}
+
+	operator := result[1]
+	a, err := strconv.Atoi(result[0])
+	b, err := strconv.Atoi(result[2])
+
+	if operator == "+" {
+		return strconv.Itoa(sum(a, b)), nil
+	} else if operator == "-" {
+		return strconv.Itoa(subtr(a, b)), nil
+	}
+
 	return "", nil
+}
+
+func StringSum(input string) (output string, err error) {
+	output, err = parse(input)
+
+	if err != nil {
+		return "", err
+	}
+
+	return output, nil
 }
